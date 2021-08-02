097736181486367513f3daf910661a6d3fceabdd
Vallem Balu <vbnreddy5@gmail.com>
2021-08-02 23:01:05 +0530

    Binary Search the Word from Word List

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -7,12 +7,33 @@ namespace Algorithm
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Algorithm Problems");
-            String str = "ABC";
-            int n = str.Length;
-            permutations_String.permute(str, 0, n - 1);
+            Console.WriteLine("Enter Option");
+            Console.WriteLine("1.Permutations_String");
+            Console.WriteLine("2.BinarySearchFile");
+
+            int Option = int.Parse(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    Console.WriteLine("Please Enter Perm_string: ");
+                    String str = Console.ReadLine();
+                    int n = str.Length;
+                    permutations_String.permute(str, 0, n - 1);
+                    break;
+                case 2:
+                    BinarySearchFile.Binarysearch();
+                    break;
+
+                default:
+                    Console.WriteLine("Please Enter valid option");
+                    break;
+
+            }
         }
 
-
     }
-}
+
+
+ }
+
 
---
+++ Algorithm/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class Utility
+    {
+        public static string usersinput()
+        {
+            return Console.ReadLine();
+        }
+        public static int binarysearch(string[] arr, string key)
+        {
+            int l = 0;
+            int h = arr.Length;
+            while (l <= h)
+            {
+                int m = l + (h - l) / 2;
+                int res = key.CompareTo(arr[m]);
+                if (res == 0)
+                    return m;
+                if (res > 0)
+                {
+                    l = m + 1;
+
+                }
+                else
+                {
+                    h = m - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
